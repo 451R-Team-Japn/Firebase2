2e2a13dd33f982ff255c9df1cc625559036e6ab3
Ambrgna <mittens.pritchett@gmail.com>
2022-10-08 00:18:01 -0500

    Update formdatabase.js

--- src/script/studentform/formdatabase.js
+++ src/script/studentform/formdatabase.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-const currentuser;
+var currentuser;
 var user;
 var applicant;
 
