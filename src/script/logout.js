efb9c491a9dd53db5439b8c0220f134222791d4a
Ambrgna <mittens.pritchett@gmail.com>
2022-10-14 23:44:11 -0500

    Delete logout.js

--- src/script/logout.js
+++
@@ -1,9 +0,0 @@
-$(document).on("click", "#logout" ,function() {
-	logout();
-	
-});function logout() {
-	localStorage.setItem("ID", null);
-	sessionStorage.setItem("ID", null);
-	
-	window.location.href = 'index.html';
-}
