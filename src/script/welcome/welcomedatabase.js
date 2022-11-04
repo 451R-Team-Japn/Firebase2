e6a5d65a4c7ae12f15fe71179867627b6df29bce
Ambrgna <mittens.pritchett@gmail.com>
2022-11-04 18:25:25 -0500

    Update welcomedatabase.js

--- src/script/welcome/welcomedatabase.js
+++ src/script/welcome/welcomedatabase.js
@@ -16,6 +16,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
 	writeCourses();
